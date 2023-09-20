a69bd9fea0d8bba50689b630498472a84dbd5256
vishaldeshmukh801 <vishaldeshmukh801@gmail.com>
2023-09-20 09:53:20 +0530

    Assignment On Variable
    Assignment On Variable.

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName; // variable declaration
+firstName = "Vishal";  // Variable initialization
+console.log(firstName);
+firstName = "Mr. Vishal"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Deshmukh"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6"; // initialization
+console.log(height);
+height = "6.1 feet";// update
+console.log(height);
---
+++ 01_variableAssignment.js
@@ -0,0 +1,24 @@
+console.log(`====================================Step-01===================================`);
+var myFullName;
+console.log(myFullName);
+myFullName="Vishal Deshmukh";
+console.log(myFullName);
+myFullName="Vishal Fattesinha Deshmukh"
+console.log(`My Full Name = ${myFullName}`);
+console.log(`================================Step-02=======================================`);
+var myLove="I Love Only Js";
+console.log(myLove);
+console.log(`================================Step-03========================================`);
+var firstName="Vishal"
+console.log(`First Name= ${firstName}`);
+var lastName="Deshmukh";
+console.log(`Last Name= ${lastName}`);
+var age=25;
+console.log(`Age=${age}`);
+var address="Pandharpur";
+console.log(`Address= ${address}`);
+var pinCode=413304;
+console.log(`Pincode= ${pinCode}`);
+var isMarried="Unmarried";
+console.log(`Marital Status= ${isMarried}`);
+console.log(`================================================================================`);
