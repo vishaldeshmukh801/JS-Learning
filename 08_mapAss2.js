32f03b3ecac767c7c781565921897bdacb93e87c
vishaldeshmukh801 <vishaldeshmukh801@gmail.com>
2023-10-16 16:05:25 +0530

    small changes in map assi
    small changes in map assi

--- 08_mapAss2.js
+++ 08_mapAss2.js
@@ -12,24 +12,25 @@ this.interestRate=interestRate;
     }
 }
 const axisBank=new Bank("Axis Bank","Pandharpur",3399167766,"Axis2234","12%");
-
+console.log(axisBank);
 const sbiBank=new Bank("SBI Bank","Solapur",2359166761,"SBI3425","10%");
-
+console.log(sbiBank);
 const iciciBank=new Bank("ICICI Bank","Pune",1339187866,"ICICI6734","14%");
-
+console.log(iciciBank);
 const kotakBank=new Bank("Kotak Bank","Mumbai",6329164766,"KO5546","11%");
-
+console.log(kotakBank);
 const hdfcBank=new Bank("HDFC Bank","Latur",9311117466,"Hdfc9091","14%");
-
+console.log(hdfcBank);
 const punjabBank=new Bank("Punjab Bank","Thane",7322217465,"PNB8091","16%");
+console.log(punjabBank);
 
 const bankMap = new Map();
-bankMap.set(3399167766,axisBank);
-bankMap.set(2359166761,sbiBank);
-bankMap.set(1339187866,iciciBank);
-bankMap.set(6329164766,kotakBank);
-bankMap.set(9311117466,hdfcBank);
-bankMap.set(7322217465,punjabBank);
+bankMap.set(axisBank.accountNumber,axisBank);
+bankMap.set(sbiBank.accountNumber,sbiBank);
+bankMap.set(iciciBank.accountNumber,iciciBank);
+bankMap.set(kotakBank.accountNumber,kotakBank);
+bankMap.set(hdfcBank.accountNumber,hdfcBank);
+bankMap.set(punjabBank.accountNumber,punjabBank);
 console.log(`---------------------------------------------- Step-02-----------------------------------------`);
 
 console.log(bankMap);
